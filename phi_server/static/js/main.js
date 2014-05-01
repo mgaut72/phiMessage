c5a1226fe8e730626de5202d49454ed8343aff8f
Chris LaRose <cjlarose@gmail.com>
2014-04-30 23:10:40 -0700

    Add SJCL to main.js

--- phi_server/static/js/main.js
+++ phi_server/static/js/main.js
@@ -4,11 +4,15 @@ require.config({
         'underscore': '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
         'moment': '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.5.1/moment.min',
         'react': '//cdnjs.cloudflare.com/ajax/libs/react/0.10.0/react',
-        'rsvp': '//cdn.jsdelivr.net/rsvp/3.0/rsvp.amd.min'
+        'rsvp': '//cdn.jsdelivr.net/rsvp/3.0/rsvp.amd.min',
+        'sjcl': '//cdnjs.cloudflare.com/ajax/libs/sjcl/1.0.0/sjcl.min'
     },
     shim: {
         underscore: {
             exports: '_'
+        },
+        sjcl: {
+            exports: 'sjcl'
         },
     }
 });
