a4f8edaed69b097c9cab73225ce76f09ae5e8813
Matt Gautreau <mgaut72@gmail.com>
2014-04-30 20:18:15 -0700

    implement /keys/<username> endpoint

--- phi_server/server.py
+++ phi_server/server.py
@@ -1,13 +1,33 @@
-from flask import Flask, request
+import collections
+import json
+from flask import Flask, Response, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/keys/<username>/', methods=['GET', 'POST'])
+database = collections.defaultdict(list)
+
+@app.route('/keys/<username>', methods=['GET', 'POST'])
 def keys(username):
     if request.method == 'POST':
-        data = get_json()
-        print data
+        data = request.get_json()
+        print "got data" + str(data)
+        try:
+            did = data['device_id']
+            rn = data['rsa']['n']
+            re = data['rsa']['e']
+            ex = data['ecdsa']['x']
+            ey = data['ecdsa']['y']
+        except:
+            return 'Malformed key object', 400
+        keys = {'device_id': did,
+                'rsa' : {'n' : rn, 'e' : re},
+                'ecdsa' : {'x' : ex, 'y' : ey}}
+        database[username].append(keys)
+        print "added " + str(keys) + " to the db"
+        return jsonify(**keys)
 
+    elif request.method == 'GET':
+        return json.dumps(database[username])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
