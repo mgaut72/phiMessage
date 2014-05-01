2d54f656c26399c5fca1b95202d9789e89a182b7
Chris LaRose <cjlarose@gmail.com>
2014-05-01 00:09:34 -0700

    Better comments

--- phi_message/static/js/rsa.js
+++ phi_message/static/js/rsa.js
@@ -1,9 +1,26 @@
 define(['jsbn/rsa', 'jsbn/rsa2'], function() {
 
     /*
-     * RSA Public Exponent
+     * Shared public exponent for RSA public-key cryptosystem
+     * http://en.wikipedia.org/wiki/RSA_(cryptosystem)
+     *
+     * Commonly called 'e', e should be relatively prime to p-1 for all primes
+     * p which divide the modulus.
+     *
+     * There is no known attack against small public exponents such as e = 3,
+     * provided that proper padding is used. Coppersmith's Attack has many
+     * applications in attacking RSA specifically if the public exponent e is
+     * small and if the encrypted message is short and not padded. 65537 is a
+     * commonly used value for e; this value can be regarded as a compromise
+     * between avoiding potential small exponent attacks and still allowing
+     * efficient encryptions (or signature verification). The NIST Special
+     * Publication on Computer Security (SP 800-78 Rev 1 of August 2007) does not
+     * allow public exponents e smaller than 65537, but does not state a reason
+     * for this restriction.
+     *
+     * Here, 'e' is encoded in base-16 and represents the value 65537
      */
-    var PUBLIC_EXPONENT = "101";
+    var PUBLIC_EXPONENT = "10001";
 
     return {
         generate: function() {
