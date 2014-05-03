956c371e9944a962598b5527570d494dcd266448
Chris LaRose <cjlarose@gmail.com>
2014-05-03 12:33:03 -0700

    Add user list

---
+++ phi_message/static/js/components/application.js
@@ -0,0 +1,39 @@
+define(['react', 'underscore'], function(React, _) {
+
+    var UserList = React.createClass({
+        render: function() {
+            return React.DOM.div({},
+                React.DOM.h2({}, "Online Users"),
+                React.DOM.ul({id: 'user-list'},
+                    _.map(this.props.users, function(user) {
+                        return React.DOM.li({key: user},
+                            React.DOM.a({href: '#'}, user));
+                    })));
+        }
+    });
+
+    var Application = React.createClass({
+        getInitialState: function() {
+            return {
+                messages: {},
+                users: []
+            };
+        },
+        componentDidMount: function() {
+            var fakeUsers = ['matt', 'taylor', 'chris', 'daniel'];
+            for (var i = 0; i < fakeUsers.length; i++) {
+                setTimeout(function(k) {
+                    var users = this.state.users;
+                    users.push(fakeUsers[k]);
+                    this.setState({users: users}); 
+                }.bind(this, i), i * 1000);
+            }
+        },
+        render: function() {
+            return React.DOM.div({id: 'application'},
+                UserList({users: this.state.users}));
+        }
+    });
+
+    return Application;
+});
