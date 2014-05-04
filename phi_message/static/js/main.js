69e0c2f5b148e4bf6783efb73347236f882d7c92
Chris LaRose <cjlarose@gmail.com>
2014-05-03 19:59:40 -0700

    Add unminified SJCL source because it is inexplicably not shitty like the minified one

--- phi_message/static/js/main.js
+++ phi_message/static/js/main.js
@@ -5,7 +5,6 @@ require.config({
         'moment': '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.5.1/moment.min',
         'react': '//cdnjs.cloudflare.com/ajax/libs/react/0.10.0/react',
         'rsvp': '//cdn.jsdelivr.net/rsvp/3.0/rsvp.amd.min',
-        'sjcl': '//cdnjs.cloudflare.com/ajax/libs/sjcl/1.0.0/sjcl.min',
         'socketio': '//cdnjs.cloudflare.com/ajax/libs/socket.io/0.9.16/socket.io.min'
     },
     shim: {
