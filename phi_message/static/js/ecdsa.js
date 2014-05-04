18466e2629fa40adf3092463241d93c287ec8558
Chris LaRose <cjlarose@gmail.com>
2014-05-03 20:46:03 -0700

    Ensure private ecdsa scalar is less than the multiplicative order n of point of prime order g

--- phi_message/static/js/ecdsa.js
+++ phi_message/static/js/ecdsa.js
@@ -1,11 +1,16 @@
 define(['jsbn/ec', 'jsbn/sec'], function(ECC, Curves) {
     return {
         generate: function() {
-            var curve = secp256r1();
-            var g = curve.getG();
+            var curveParams = secp256r1();
+            var g = curveParams.getG();
+            var n = curveParams.getN();
 
             var rng = new SecureRandom();
+
+            // choose random  k [0,n-1]
             var k = new BigInteger(256, 1, rng);
+            while (k.compareTo(n) > 0)
+                k = new BigInteger(256, 1, rng);
 
             var publicKey = g.multiply(k);
             return {
