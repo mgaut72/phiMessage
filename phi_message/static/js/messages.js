82418812140737a0039d9a83d9ed47c08dd69cf5
Chris LaRose <cjlarose@gmail.com>
2014-05-03 20:20:39 -0700

    Messages and keys now encrypted

--- phi_message/static/js/messages.js
+++ phi_message/static/js/messages.js
@@ -1,14 +1,34 @@
-define([], function() {
+define(['sjcl', 'jsbn/ec', 'jsbn/rng', 'jsbn/rsa'], function(SJCL, EC, RNG, RSA) {
 
-    function encrypt(message, senderKeys, recipientKeys) {
-        console.log(arguments);
+    var rng = new SecureRandom();
+
+    /*
+     * message is padded with ??? and encrypted with AES running in CCM mode
+     * key is padded with PKCS#1 and encrypted with RSA
+     */
+    function encrypt(plaintext, senderKeys, recipientKeys) {
+
+        // generate a random 128-bit AES key
+        var k = new BigInteger(128, 1, rng);
+        var AESKeyHex = k.toRadix(16);
+        var AESKeyBitArray = sjcl.codec.hex.toBits(AESKeyHex);
+        console.log(AESKeyHex);
+
+        // encrypt message with AES
+        var ciphertext = sjcl.encrypt(AESKeyBitArray, plaintext);
+        console.log(ciphertext);
+
+        // encrypt key with RSA
+        var encryptedKey = recipientKeys.rsa.encrypt(AESKeyHex);
+        console.log(encryptedKey);
+
         return {
             message: {
-                content: '',
+                content: ciphertext,
                 signature: ''
             },
             key: {
-                content: '',
+                content: encryptedKey,
                 signature: ''
             }
         }
