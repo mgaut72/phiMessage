d321b5418d7ba053fd7d47a7206fcf048eb53a96
Chris LaRose <cjlarose@gmail.com>
2014-05-03 20:32:51 -0700

    Fix dependency typo in main.js

--- phi_message/static/js/main.js
+++ phi_message/static/js/main.js
@@ -18,7 +18,7 @@ require.config({
         'jsbn/rsa': ['jsbn/jsbn', 'jsbn/rng'],
         'jsbn/rsa2': ['jsbn/jsbn2', 'jsbn/rsa'],
         'jsbn/jsbn2': ['jsbn/jsbn'],
-        'json/ec': ['jsbn/jsbn2']
+        'jsbn/ec': ['jsbn/jsbn2']
     }
 });
 
